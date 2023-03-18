050c4780564d6d68a3497f150b18e5ae6d13d235
Jacob Ridenour <112583892+jacobRidenour@users.noreply.github.com>
2023-03-18 11:10:12 -0400

    updated __init__
    open the GUI from the initializer class

--- __init__.py
+++ __init__.py
@@ -1,7 +1,12 @@
-
 # Will set up later ...
 # add a new dependency with the command:
 # poetry add packageName
 
 # Current dependencies
 # PyQt5
+
+# add other files in the same folder
+import gui
+
+def __init__(self, parent=None):
+    gui.MainWindow()
